04e50c4a70720501a9795a5be496a570088c674c
JoGro-code <78618089+JoGro-code@users.noreply.github.com>
2022-10-08 13:36:38 +0200

    add Input Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+//import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>GooglePlaces</h1>
+      <input
+        ngx-google-places-autocomplete
+        #placesRef="ngx-places"
+        (onAddressChange)="handleAddressChange($event)"
+      />
+      <p></p>
+      <button class="btn btn-primary" (click)="onSearch()">Suchen</button>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'auto-complete';
+
+  onSearch() {
+    alert('suche!');
+  }
+  public handleAddressChange(address: any) {
+    alert(address);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, GooglePlaceModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
